87e42e8905aa9fa8d30566bc8b384ac7545cc0c9
Pubudu Arosha <it18108514@my.sliit.lk>
2021-03-11 19:51:17 +0530

    updated

--- ValidPrescription/src/validprescription/ValidPrescriptionActivator.java
+++ ValidPrescription/src/validprescription/ValidPrescriptionActivator.java
@@ -2,29 +2,29 @@ package validprescription;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import prescriptionavailability.PrescriptionAvailability;
+
 
 public class ValidPrescriptionActivator implements BundleActivator {
 
-	private static BundleContext context;
+	ServiceReference serviceReference;
 
-	static BundleContext getContext() {
-		return context;
+	@Override
+	public void start(BundleContext context) throws Exception {
+
+		System.out.println("Valid Prescription Service");
+		serviceReference = context.getServiceReference(PrescriptionAvailability.class
+				.getName());
+		PrescriptionAvailability servicePublish = (PrescriptionAvailability) context
+				.getService(serviceReference);
+		System.out.println(servicePublish.messageService("Message from Client"));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		ValidPrescriptionActivator.context = bundleContext;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		ValidPrescriptionActivator.context = null;
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Good Bye !!!");
+		context.ungetService(serviceReference);
 	}
 
 }
